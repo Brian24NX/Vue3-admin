0a02278c96cf7297df36ef6d884f611bbc76dabb
Brian <872696379@qq.com>
2022-12-01 13:34:13 +0800

    curdConfiguration

--- src/views/user/roles.vue
+++ src/views/user/roles.vue
@@ -1,95 +1,70 @@
 <template>
-    <t-card>
-      <div class="action-area">
-        <t-button
-          v-permission="PermissionEnum.USER_ROLES_CREATE"
-          @click="handleCreate"
-          >Create Role
-        </t-button>
-      </div>
-      <div class="search-area">
-        <t-input
-          class="search-input"
-          v-model="searchKey.name"
-          placeholder="Please type your Role name"
-        ></t-input>
-        <t-input
-          class="search-input"
-          v-model="searchKey.label"
-          placeholder="Please type your Role label"
-        ></t-input>
-        <t-button @click="fetchData">
-          <template #icon>
-            <icon name="search"></icon>
-          </template>
-        </t-button>
-      </div>
-      <t-table
-        :loading="loading"
-        row-key="index"
-        :columns="columns"
-        :data="data"
-        :pagination="pagination"
-        @page-change="onPageChange"
-      >
-        <template #operation="slotProps">
-          <t-button
-            v-permission="PermissionEnum.USER_LIST_EDIT"
-            variant="text"
-            theme="primary"
-            @click="handleEdit(slotProps)"
-          >
-            <icon name="edit"></icon>
-            Edit
-          </t-button>
+  <t-card>
+    <div class="action-area">
+      <t-button v-permission="PermissionEnum.USER_ROLES_CREATE" @click="handleCreate">Create Role
+      </t-button>
+    </div>
+    <div class="search-area">
+      <t-input class="search-input" v-model="searchKey.name" placeholder="Please type your Role name"></t-input>
+      <t-input class="search-input" v-model="searchKey.label" placeholder="Please type your Role label"></t-input>
+      <t-button @click="fetchData">
+        <template #icon>
+          <icon name="search"></icon>
         </template>
-      </t-table>
-    </t-card>
-    <role-edit-dialog
-      :show="showDialog"
-      :data="editData"
-      @close="onDialogClose"
-      @confirm="handleConfirm"
-    ></role-edit-dialog>
-  </template>
+      </t-button>
+    </div>
+    <t-table :loading="loading" row-key="index" :columns="columns" :data="data" :pagination="pagination"
+      @page-change="onPageChange">
+      <template #operation="slotProps">
+        <t-button v-permission="PermissionEnum.USER_LIST_EDIT" variant="text" theme="primary"
+          @click="handleEdit(slotProps)">
+          <icon name="edit"></icon>
+          Edit
+        </t-button>
+      </template>
+    </t-table>
+  </t-card>
+  <role-edit-dialog :show="showDialog" :data="editData" @close="onDialogClose"
+    @confirm="handleConfirm"></role-edit-dialog>
+</template>
   
-  <script lang="ts" setup>
-  import { PermissionEnum } from "@/config/permission.config";
-  import { useEditDialog } from "@/composables/useEditDialog";
-  import type { RoleCreateRequest, RoleType } from "@/api/types";
-  import roleApi from "@/api/role";
-  import { reactive } from "vue";
-  import { useSearch } from "@/composables/useSearch";
-  import RoleEditDialog from "./role-edit-dialog.vue";
-  import { Icon } from "tdesign-vue-next";
-  
-  const columns = [
-    { colKey: "id", title: "ID" },
-    { colKey: "name", title: "Role name" },
-    { colKey: "label", title: "Role label" },
-    { colKey: "operation", title: "Operation" },
-  ];
-  const searchKey = reactive({
-    name: "",
-    label: "",
-  });
-  
-  const {
-    showDialog,
-    editData,
-    handleCreate,
-    onDialogClose,
-    handleEdit,
-    handleConfirm,
-  } = useEditDialog<RoleType, RoleCreateRequest>(roleApi, "Role");
-  
-  const { data, fetchData, pagination, loading, onPageChange } = useSearch<
-    RoleType,
-    {
-      name: string;
-      label: string;
-    }
-  >(roleApi, searchKey);
+<script lang="ts" setup>
+import { PermissionEnum } from "@/config/permission.config";
+import { useEditDialog } from "@/composables/useEditDialog";
+import type { RoleCreateRequest, RoleType } from "@/api/types";
+import roleApi from "@/api/role";
+import { reactive } from "vue";
+import { useSearch } from "@/composables/useSearch";
+import RoleEditDialog from "./role-edit-dialog.vue";
+import { Icon } from "tdesign-vue-next";
+
+const columns = [
+  { colKey: "id", title: "ID" },
+  { colKey: "name", title: "Role name" },
+  { colKey: "label", title: "Role label" },
+  { colKey: "operation", title: "Operation" },
+];
+const searchKey = reactive({
+  name: "",
+  label: "",
+});
+
+const {
+  showDialog,
+  editData,
+  handleCreate,
+  onDialogClose,
+  handleEdit,
+  handleConfirm,
+} = useEditDialog<RoleType, RoleCreateRequest>(roleApi, "Role");
+
+const { data, fetchData, pagination, loading, onPageChange } = useSearch<
+  RoleType,
+  {
+    name: string;
+    label: string;
+  }
+>(roleApi, searchKey);
 </script>
 
 
