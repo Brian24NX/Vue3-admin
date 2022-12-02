a26e8c650ceb0b29e15fc0554480b40e90103172
Brian <872696379@qq.com>
2022-12-02 13:58:47 +0800

    revise_nodeVersion_baseURL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/Vue3-project/",
   plugins: [vue(), splitVendorChunkPlugin()],
   resolve: {
     alias: {
