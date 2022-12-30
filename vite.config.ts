9aa03f289bdefb967153800186137a488fff5e27
Brian Zhou <109346901+Brian24NX@users.noreply.github.com>
2022-12-31 04:38:38 +0800

    change baseURL to Vue3-admin

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Vue3-project/",
+  base: "/Vue3-admin/",
   plugins: [vue(), splitVendorChunkPlugin()],
   resolve: {
     alias: {
