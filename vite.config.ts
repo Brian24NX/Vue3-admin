49bad30da31d0345df9377005935777d00a1c286
Brian Zhou <109346901+Brian24NX@users.noreply.github.com>
2022-12-31 04:42:50 +0800

    Vue3-project

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Vue3-admin/",
+  base: "/Vue3-project/",
   plugins: [vue(), splitVendorChunkPlugin()],
   resolve: {
     alias: {
