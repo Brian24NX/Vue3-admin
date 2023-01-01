d934e173fdbfd375c72631ab5c7daaf57da222a7
Brian <872696379@qq.com>
2023-01-01 08:06:12 +0800

    baseURL added

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/Vue3-admin/",
   plugins: [vue(), splitVendorChunkPlugin()],
   resolve: {
     alias: {
